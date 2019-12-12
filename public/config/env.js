b69d458a2a8833971b487e4bf07406a9bff4a6d4
diazcoin <diazcoin1@gmail.com>
2019-12-12 21:44:40 +0100

    added

--- public/config/env.js
+++ public/config/env.js
@@ -1,15 +1,15 @@
 window.env = {
     api: {
-        authUrl: 'http://localhost:9002/api/v2/barong',
-        tradeUrl: 'http://localhost:9002/api/v2/peatio',
-        applogicUrl: 'http://localhost:9002/api/v2/applogic',
-        rangerUrl: 'ws://localhost:9003/api/v2/ranger',
+        authUrl: 'https://crypto2exchange.online/api/v2/barong',
+        tradeUrl: 'https://crypto2exchange.online/api/v2/peatio',
+        applogicUrl: 'https://crypto2exchange.online/api/v2/applogic',
+        rangerUrl: 'wss://crypto2exchange.online/api/v2/ranger',
     },
     minutesUntilAutoLogout: '5',
     withCredentials: false,
     captcha: {
-        captchaType: 'none',
-        siteKey: '',
+        captchaType: 'images',
+        siteKey: '6LfTasEUAAAAAOnOZG9TixPQ-FiZKgyU3ej4Gs45',
     },
     gaTrackerKey: '',
     rangerReconnectPeriod: '1',
